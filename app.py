bd4372f7c1a094d4dc823f4f17f989713a45efa6
Keith Oshima <koshima789@gmail.com>
2021-10-05 22:02:41 -0700

    added s3 bucket storage. can redirect to file url. need to work on rest/http api

--- app.py
+++ app.py
@@ -1,31 +1,51 @@
 import os
+import boto3
+from botocore.client import Config
 import base64
 from sccrpt import SCCReport
+
+s3 = boto3.resource('s3')
+client = boto3.client('s3')
 
 
 def handler(event, context):
     option = event.get("option")
+    bucket = s3.Bucket("esccrpt")
+
     rpt = SCCReport()
+
     if option == "rpt":
         path = rpt.report
-        content_type = "image/png"
     elif option == "opdf":
         path = rpt.recent_pdf_report
-        content_type = "application/pdf"
     else:
         path = rpt.recent_txt_report[0]
-        content_type = "text/plain"
+
+    # get filename
+    _, fname = os.path.split(path)
 
     if rpt.BYTES_ONLY:
         # convert to bytes, send file content, and remove.
         with open(path, 'rb') as fobj:
-            file_content = base64.b64encode(fobj.read())
+            client.upload_fileobj(fobj, bucket.name, fname)
 
+        # remove file from tmp/___
         os.remove(path)
 
+        # generate presigned url for temp access to s3 file.
+        url = client.generate_presigned_url(
+            ClientMethod='get_object',
+            Params={
+                'Bucket': bucket.name,
+                'Key': fname
+            },
+            ExpiresIn=604800  # 7 days
+        )
+
+        # redirect url
         return {
-            'isBase64Encoded': True,
-            'statusCode': 200,
-            'headers': {'Content-Type': content_type},
-            'body': file_content.decode("utf-8")
+            'statusCode': 302,
+            'headers': {
+                'Location': url
+            }
         }
